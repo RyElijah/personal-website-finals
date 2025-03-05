96810e10499e4002eec75a0a48af58000d10a880
RyElijah <rsluar@student.apc.edu.ph>
2025-03-05 19:50:29 +0000

    qweqwe

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import PersonalProfile from './components/PersonalProfile.vue';
-import 'swiper/css/swiper-bundle.min.css'; // Import Swiper CSS
 
 const app = createApp(App);
 app.component('personal-profile', PersonalProfile);
