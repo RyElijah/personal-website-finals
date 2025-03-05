70496d50bd6ab1b32a1e81da06d468beb4f571c1
RyElijah <rsluar@student.apc.edu.ph>
2025-03-05 19:33:00 +0000

    game na po?

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import PersonalProfile from './components/PersonalProfile.vue';
+import 'swiper/css/swiper-bundle.min.css'; // Import Swiper CSS
 
 const app = createApp(App);
 app.component('personal-profile', PersonalProfile);
