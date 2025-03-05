5d5768e6442bdbc66a6f97513d8fcee8d7d2c0eb
RyElijah <rsluar@student.apc.edu.ph>
2025-03-05 19:08:04 +0000

    sige na please

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,10 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import PersonalProfile from './components/PersonalProfile.vue';
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/bundle';
-
 const app = createApp(App);
 app.component('personal-profile', PersonalProfile);
 
